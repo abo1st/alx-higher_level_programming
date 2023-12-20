c670bad75936d5950e89c82633924483d028d059
abo1st <abo1st001@gmail.com>
2023-12-19 17:14:07 -0800

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+0-square.py
+#!/usr/bin/python3
+
+""This defines a class Square."""
+
+
+class Square:
+    """This represent a square."""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+This project is about 0x06-python-classes.
+
